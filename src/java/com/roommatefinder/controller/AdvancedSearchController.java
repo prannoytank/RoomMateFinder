06d88d67b54f9bc00304c3284503d8923f0249fb
Diana Alexander <dia.alexander@gmail.com>
2015-12-05 16:13:35 -0500

    Advanced search functionality

---
+++ src/java/com/roommatefinder/controller/AdvancedSearchController.java
@@ -0,0 +1,89 @@
+package com.roommatefinder.controller;
+import com.roommatefinder.model.Advertisment;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ *
+ * @author DianaA
+ */
+
+@Controller
+@RequestMapping("/search")
+public class AdvancedSearchController {
+ 
+ @RequestMapping(method = RequestMethod.GET)
+ public String initForm(Model model) {
+        Advertisment adModel1 = new Advertisment();
+        model.addAttribute("adModel1", adModel1);
+         initModelList(model);
+        return "pages/home/advancedSearch";
+    }
+ 
+ private void initModelList(Model model){
+    
+     List<String> cities = new ArrayList<String>();
+     cities.add("Toronto");
+     cities.add("Montreal");
+     cities.add("London");
+     cities.add("Vancouver");
+     cities.add("Calgary");
+     cities.add("Winnipeg");
+     model.addAttribute("cities", cities);
+     
+     List<String> gender = new ArrayList<String>();
+     gender.add("Male");
+     gender.add("Female");
+     gender.add("Other");
+     model.addAttribute("gender", gender);
+     
+     List<String> buildingType = new ArrayList<String>();
+     buildingType.add("House");
+     buildingType.add("Apartment");
+     buildingType.add("Condo");
+     model.addAttribute("buildingType", buildingType);
+     
+     List<Integer> noOfRooms = new ArrayList<Integer>();
+     noOfRooms.add(1);
+     noOfRooms.add(2);
+     noOfRooms.add(3);
+     noOfRooms.add(4);
+     noOfRooms.add(5);
+     model.addAttribute("noOfRooms", noOfRooms);
+     
+        
+ }
+ 
+ @RequestMapping(method = RequestMethod.POST)
+	public String submitForm(Model model,@ModelAttribute("adModel1") @Valid Advertisment adModel1, BindingResult result) {
+              
+		model.addAttribute("adModel1",adModel1);
+		String returnVal = "successForm";
+		if(result.hasErrors()) {
+			returnVal = "pages/home/advancedSearch";
+		} else {
+                model.addAttribute("adModel1", adModel1);
+                
+                }		
+		return returnVal;
+	}   
+
+}
+
+    
+
--- src/java/com/roommatefinder/controller/AdvertismentController.java
+++ src/java/com/roommatefinder/controller/AdvertismentController.java
@@ -7,6 +7,8 @@ package com.roommatefinder.controller;
 
 import com.roommatefinder.model.Advertisment;
 import com.roommatefinder.validator.AdvertismentValidator;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +23,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 /**
  *
  * @author AshirwadTank
@@ -33,41 +34,69 @@ public class AdvertismentController {
     //@Autowired
     // @Qualifier("formValidator")
     // private Validator validator;
-    
-     @Autowired
+    @Autowired
     @Qualifier("advertismentValidator")
     private AdvertismentValidator validator;
-    
+
     @InitBinder
     private void initBinder(WebDataBinder binder) {
         binder.setValidator(validator);
     }
-    
-    
+
     @RequestMapping(method = RequestMethod.GET)
     public String initForm(Model model) {
         Advertisment adModel = new Advertisment();
 
         model.addAttribute("adModel", adModel);
-
+        initModelList(model);
         return "pages/home/advertisment";
     }
-    
-    
- @RequestMapping(method = RequestMethod.POST)
-	public String submitForm(Model model,@ModelAttribute("adModel") @Valid Advertisment adModel, BindingResult result) {
-              
-		model.addAttribute("adModel",adModel);
-		String returnVal = "successForm";
-		if(result.hasErrors()) {
-			returnVal = "pages/home/advertisment";
-		} else {
-                  
-                    
-			model.addAttribute("adModel", adModel);
-                        
-		}		
-		return returnVal;
-	}   
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(Model model, @ModelAttribute("adModel") @Valid Advertisment adModel, BindingResult result) {
+
+        model.addAttribute("adModel", adModel);
+        String returnVal = "successForm";
+        if (result.hasErrors()) {
+            returnVal = "pages/home/advertisment";
+        } else {
+            model.addAttribute("adModel", adModel);
+
+        }
+        return returnVal;
+    }
+
+    private void initModelList(Model model) {
+
+        List<String> cities = new ArrayList<String>();
+        cities.add("Toronto");
+        cities.add("Montreal");
+        cities.add("London");
+        cities.add("Vancouver");
+        cities.add("Calgary");
+        cities.add("Winnipeg");
+        model.addAttribute("cities", cities);
+
+        List<String> gender = new ArrayList<String>();
+        gender.add("Male");
+        gender.add("Female");
+        gender.add("Other");
+        model.addAttribute("gender", gender);
+
+        List<String> buildingType = new ArrayList<String>();
+        buildingType.add("House");
+        buildingType.add("Apartment");
+        buildingType.add("Condo");
+        model.addAttribute("buildingType", buildingType);
+
+        List<Integer> noOfRooms = new ArrayList<Integer>();
+        noOfRooms.add(1);
+        noOfRooms.add(2);
+        noOfRooms.add(3);
+        noOfRooms.add(4);
+        noOfRooms.add(5);
+        model.addAttribute("noOfRooms", noOfRooms);
+
+    }
 
 }
